17245e35ce992780a2fc8002ab5e9bef8890942d
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-03-09 11:57:16 +0100

    Add basic home-manager config

---
+++ home.nix
@@ -0,0 +1,94 @@
+{ config, pkgs, ... }: {
+
+  # Lookup all home-manager options here:
+  # https://rycee.gitlab.io/home-manager/options.html
+
+  # Allow "unfree" licenced packages in home-manager
+  nixpkgs.config.allowUnfree = true;
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    htop
+    zellij
+    vivaldi
+    wezterm
+    zettlr
+    tmux
+    signal-desktop
+    slack
+    libreoffice-qt
+    hunspell
+    hunspellDicts.de_DE
+    hunspellDicts.en_US-large
+    hunspellDicts.es_MX
+    firefox
+    gnome.pomodoro
+    onedrive
+    picocom
+    spotify
+  ];
+
+
+  programs = {
+
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
+    # Neovim
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+
+      plugins = with pkgs.vimPlugins; [
+        vim-nix
+      ];
+
+      vimAlias = true;
+      viAlias = true;
+      vimdiffAlias = true;
+    };
+
+    # Shell (ZSH)
+    zsh = {
+      enable = true;
+      historySubstringSearch.enable = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+      autocd = true;
+
+      prezto = {
+        enable = true;
+        prompt.theme = "pure";
+
+        # Case insensitive completion
+        caseSensitive = false;
+
+        # Autoconvert .... to ../..
+        editor.dotExpansion = true;
+
+        # Prezto modules to load
+        pmodules = [ "utility" "editor" "directory" "prompt" ];
+
+        terminal.autoTitle = true;
+      };
+    };
+
+    # Other
+    zellij.enable = true;
+    tealdeer.enable = true;
+  };
+
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+
+
+}
